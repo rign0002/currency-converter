76d1c0921bd26e8bd5759707cfd4f8e00bc98df4
Justin <rign0002@algonquinlive.com>
2019-06-26 23:08:06 -0300

    added a footer

--- js/exchange.js
+++ js/exchange.js
@@ -6,5 +6,12 @@ import currencies from "./currencies.js";
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-    
+    copyrightMessage()
+}
+
+const copyrightMessage = () => {
+    const date = new Date()
+    let year = date.getFullYear()
+    const copyrightTag = document.querySelector('#copyright')
+    copyrightTag.innerHTML = `&copy; Copyright ${year} Justin Rignault. All Rights Reserved`
 }
